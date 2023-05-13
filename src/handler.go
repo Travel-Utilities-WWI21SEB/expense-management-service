c5c5767f39d56fd4df73df06080a5a5f1ff6f6f1
Kevin Rieger <kevin.rieger@sap.com>
2023-05-13 15:31:43 +0200

    feat: add basic gin routing

--- src/handler.go
+++ src/handler.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/Travel-Utilities-WWI21SEB/expense-management-service/src/model"
+)
+
+func LifeCheckHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		response := model.LifeCheckResponse{
+			Alive:   true,
+			Version: "1.0.0",
+		}
+
+		c.JSON(http.StatusOK, response)
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		hostname, _ := os.Hostname()
-		fmt.Fprintf(w, "Hello World from %s", hostname)
-	})
+	router := createRouter()
 
-	http.ListenAndServe(":8080", nil)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting or closing listener:: %v", err)
+			os.Exit(0)
+		}
+	}()
+
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	if err := server.Shutdown(context.TODO()); err != nil {
+		log.Fatalf("Error closing server:: %v", err)
+	}
+
+	log.Println("Server stopped gracefully")
+	os.Exit(0)
 }
