1f539673db568dd4aa031d51d4b8a894135ff02a
Frecherenkel60 <rieger.kevin01@gmail.com>
2023-05-22 09:45:01 +0200

    feat: implement registration and login endpoint

--- src/model/usermodels.go
+++ src/model/usermodels.go
@@ -4,6 +4,17 @@ import (
 	"github.com/google/uuid"
 )
 
+type RegistrationRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"username"`
+	Password string `json:"password"`
+}
+
 type UserResponse struct {
 	UserID *uuid.UUID `json:"userId"`
 }
