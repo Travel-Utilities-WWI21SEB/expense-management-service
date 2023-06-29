e16c89d472f5b43c8863a52ad8ec0bab4995e0fa
930C <93C@pm.me>
2023-06-29 18:33:29 +0200

    feat: add GET debts endpoint

--- src/handlers/debt_handler.go
+++ src/handlers/debt_handler.go
@@ -2,12 +2,28 @@ package handlers
 
 import (
 	"github.com/Travel-Utilities-WWI21SEB/expense-management-service/src/controllers"
+	"github.com/Travel-Utilities-WWI21SEB/expense-management-service/src/models"
+	"github.com/Travel-Utilities-WWI21SEB/expense-management-service/src/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
 )
 
 func GetDebtsHandler(DebtCtl controllers.DebtCtl) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// TO-DO
+		ctx := c.Request.Context()
+
+		// Get tripId from request params
+		tripId := uuid.MustParse(c.Param(models.ExpenseParamKeyTripId))
+
+		// Get debts
+		response, err := DebtCtl.GetDebtEntries(ctx, &tripId)
+		if err != nil {
+			utils.HandleErrorAndAbort(c, *err)
+			return
+		}
+
+		c.JSON(http.StatusOK, response)
 	}
 }
 
