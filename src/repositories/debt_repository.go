450cda83df76e9087ff60863fc6dcd64208932dc
930C <93C@pm.me>
2023-07-08 10:00:48 +0200

    fix: add transactions to debt queries

--- src/repositories/debt_repository.go
+++ src/repositories/debt_repository.go
@@ -14,13 +14,12 @@ import (
 
 type DebtRepo interface {
 	GetDebtById(ctx context.Context, debtId *uuid.UUID) (*models.DebtSchema, *models.ExpenseServiceError)
-	GetDebts(ctx context.Context) ([]*models.DebtSchema, *models.ExpenseServiceError)
 	AddTx(ctx context.Context, tx pgx.Tx, debt *models.DebtSchema) *models.ExpenseServiceError
 	UpdateTx(ctx context.Context, tx pgx.Tx, debt *models.DebtSchema) *models.ExpenseServiceError
 	DeleteTx(ctx context.Context, tx pgx.Tx, debtId *uuid.UUID) *models.ExpenseServiceError
 
 	GetDebtByCreditorId(ctx context.Context, creditorId *uuid.UUID) (*models.DebtSchema, *models.ExpenseServiceError)
-	GetDebtByCreditorIdAndDebtorIdAndTripId(ctx context.Context, creditorId *uuid.UUID, debtorId *uuid.UUID, tripId *uuid.UUID) (*models.DebtSchema, *models.ExpenseServiceError)
+	GetDebtByCreditorIdAndDebtorIdAndTripIdTx(ctx context.Context, tx pgx.Tx, creditorId *uuid.UUID, debtorId *uuid.UUID, tripId *uuid.UUID) (*models.DebtSchema, *models.ExpenseServiceError)
 	GetDebtEntriesByTripId(ctx context.Context, tripId *uuid.UUID) ([]*models.DebtSchema, *models.ExpenseServiceError)
 
 	CalculateDebt(ctx context.Context, tx pgx.Tx, creditorId *uuid.UUID, debtorId *uuid.UUID, tripId *uuid.UUID, amountToAdd decimal.Decimal) *models.ExpenseServiceError
@@ -46,28 +45,6 @@ func (dr *DebtRepository) GetDebtById(ctx context.Context, debtId *uuid.UUID) (*
 	return debt, nil
 }
 
-func (dr *DebtRepository) GetDebts(ctx context.Context) ([]*models.DebtSchema, *models.ExpenseServiceError) {
-	query := "SELECT * FROM debt"
-	rows, err := dr.DatabaseMgr.ExecuteQuery(ctx, query)
-	if err != nil {
-		return nil, expense_errors.EXPENSE_BAD_REQUEST
-	}
-	defer rows.Close()
-
-	var debts []*models.DebtSchema
-	for rows.Next() {
-		debt := &models.DebtSchema{}
-		err := rows.Scan(&debt.DebtID, &debt.CreditorId, &debt.DebtorId, &debt.TripId, &debt.Amount, &debt.CurrencyCode, &debt.CreationDate, &debt.UpdateDate)
-		if err != nil {
-			return nil, expense_errors.EXPENSE_BAD_REQUEST
-		}
-
-		debts = append(debts, debt)
-	}
-
-	return debts, nil
-}
-
 func (*DebtRepository) AddTx(ctx context.Context, tx pgx.Tx, debt *models.DebtSchema) *models.ExpenseServiceError {
 	query := "INSERT INTO debt (id, id_creditor, id_debtor, id_trip, amount, currency_code, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"
 	_, err := tx.Exec(ctx, query, debt.DebtID, debt.CreditorId, debt.DebtorId, debt.TripId, debt.Amount, debt.CurrencyCode, debt.CreationDate, debt.UpdateDate)
@@ -81,6 +58,7 @@ func (*DebtRepository) AddTx(ctx context.Context, tx pgx.Tx, debt *models.DebtSc
 func (*DebtRepository) UpdateTx(ctx context.Context, tx pgx.Tx, debt *models.DebtSchema) *models.ExpenseServiceError {
 	query := "UPDATE debt SET id_creditor = $1, id_debtor = $2, id_trip = $3, amount = $4, currency_code = $5, updated_at = $6 WHERE id = $7"
 	_, err := tx.Exec(ctx, query, debt.CreditorId, debt.DebtorId, debt.TripId, debt.Amount, debt.CurrencyCode, debt.UpdateDate, debt.DebtID)
+	log.Printf("Updated debt: %v", debt.Amount)
 	if err != nil {
 		return expense_errors.EXPENSE_BAD_REQUEST
 	}
@@ -111,11 +89,12 @@ func (dr *DebtRepository) GetDebtByCreditorId(ctx context.Context, creditorId *u
 	return debt, nil
 }
 
-func (dr *DebtRepository) GetDebtByCreditorIdAndDebtorIdAndTripId(ctx context.Context, creditorId *uuid.UUID, debtorId *uuid.UUID, tripId *uuid.UUID) (*models.DebtSchema, *models.ExpenseServiceError) {
+func (*DebtRepository) GetDebtByCreditorIdAndDebtorIdAndTripIdTx(ctx context.Context, tx pgx.Tx, creditorId *uuid.UUID, debtorId *uuid.UUID, tripId *uuid.UUID) (*models.DebtSchema, *models.ExpenseServiceError) {
+	// It is important to use transaction here, because we are in the middle of a transaction
 	query := "SELECT id, id_creditor, id_debtor, id_trip, amount, currency_code, created_at, updated_at FROM debt WHERE id_creditor = $1 AND id_debtor = $2 AND id_trip = $3"
-	row := dr.DatabaseMgr.ExecuteQueryRow(ctx, query, creditorId, debtorId, tripId)
-	debt := &models.DebtSchema{}
+	row := tx.QueryRow(ctx, query, creditorId, debtorId, tripId)
 
+	debt := &models.DebtSchema{}
 	err := row.Scan(&debt.DebtID, &debt.CreditorId, &debt.DebtorId, &debt.TripId, &debt.Amount, &debt.CurrencyCode, &debt.CreationDate, &debt.UpdateDate)
 	if err != nil {
 		return nil, expense_errors.EXPENSE_BAD_REQUEST
@@ -154,7 +133,7 @@ func (dr *DebtRepository) CalculateDebt(ctx context.Context, tx pgx.Tx, creditor
 	}
 
 	now := time.Now()
-	debt, repoErr := dr.GetDebtByCreditorIdAndDebtorIdAndTripId(ctx, creditorId, debtorId, tripId)
+	debt, repoErr := dr.GetDebtByCreditorIdAndDebtorIdAndTripIdTx(ctx, tx, creditorId, debtorId, tripId)
 	if repoErr != nil {
 		return repoErr
 	}
@@ -166,7 +145,7 @@ func (dr *DebtRepository) CalculateDebt(ctx context.Context, tx pgx.Tx, creditor
 		return repoErr
 	}
 
-	otherDebt, repoErr := dr.GetDebtByCreditorIdAndDebtorIdAndTripId(ctx, debtorId, creditorId, tripId)
+	otherDebt, repoErr := dr.GetDebtByCreditorIdAndDebtorIdAndTripIdTx(ctx, tx, debtorId, creditorId, tripId)
 	if repoErr != nil {
 		return repoErr
 	}
